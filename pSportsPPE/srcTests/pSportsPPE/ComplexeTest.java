db5af1681eec4498274e31a829169570bffc7285
fedini_doigniaux <fedini_doigniaux@HOS2UC06.sio.fr>
2018-10-10 09:43:13 +0200

    test unitaire bis

--- pSportsPPE/srcTests/pSportsPPE/ComplexeTest.java
+++ pSportsPPE/srcTests/pSportsPPE/ComplexeTest.java
@@ -2,29 +2,37 @@ package pSportsPPE;
 
 import static org.junit.Assert.*;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import fr.rigaud.Arrivee;
+import fr.rigaud.ChoixCouleur;
 import fr.rigaud.Complexe;
 
 public class ComplexeTest {
 	
 	private Complexe leComplexe;
 	private Arrivee lArrivee;
+	private Arrivee lArrivee2;
 	private ArrayList<Arrivee> lesArrivees;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		final int NBTOTALPLACEMUSCU = 4;
 		final int NBTOTALPLACEFIT = 5;
-		final char CHOIXSPORT = 'M';
+		final char MUSCU = 'M';
+		final char FIT = 'F';
 		
 		leComplexe = new Complexe(NBTOTALPLACEMUSCU, NBTOTALPLACEFIT, "leComplexe");
-		lArrivee = new Arrivee(leComplexe, CHOIXSPORT);
+		lArrivee = new Arrivee(leComplexe, MUSCU);
+		lArrivee2 = new Arrivee(leComplexe, FIT);
 		List<Arrivee> lesArrivees = new ArrayList<Arrivee>();
 		
 	}
@@ -40,12 +48,15 @@ public class ComplexeTest {
 	@Test
 	public void testEntreeUsager() {
 		assertEquals(true, leComplexe.entreeUsager(lArrivee));
+		assertEquals(true, leComplexe.entreeUsager(lArrivee2));
 	}
 	
 	@Test
 	public void testSortieUsager() {
 		assertEquals(true, leComplexe.entreeUsager(lArrivee));
+		assertEquals(true, leComplexe.entreeUsager(lArrivee2));
 		assertEquals(lArrivee, leComplexe.sortieUsager(1));
+		assertEquals(lArrivee2, leComplexe.sortieUsager(2));
 	}
 	
 	@Test
@@ -54,6 +65,44 @@ public class ComplexeTest {
 		assertEquals(0, leComplexe.getNbPlacesOccupeesMuscu());
 		assertEquals(5, leComplexe.getNbPlacesRestantesFit());
 		assertEquals(4, leComplexe.getNbPlacesRestantesMuscu());
+	}
+	
+	@Test
+	public void testInfos() {
+		final String MSGNOM = "Etat du complexe : ";
+		final String MSGDATE = "date : ";
+		final String MSGHEURE = "heure : ";
+		final String MSGDISPMUSCU = "Places disponibles M : ";
+		final String MSGDISPFIT = "Places disponibles F : ";
+		final String MSGOCCMUSCU = "Places occupées M : ";
+		final String MSGOCCFIT = "Places occupées F : ";
+		final String MSGTXMUSCU = "Taux occ. M : ";
+		final String MSGTXFIT = "Taux occ. F : ";
+		final String MSGCOULMUSCU = "Couleur M : ";
+		final String MSGCOULFIT = "Couleur F : ";
+		final String MSGBAS = "M : en musculation	F : en fitness";
+		
+		String lesInfos;
+		
+		DecimalFormat df2 = new DecimalFormat("##0.00%");
+		lesInfos = MSGNOM + "leComplexe" + "\t";
+		
+		Date laDate = Calendar.getInstance().getTime();
+		SimpleDateFormat leJour = new SimpleDateFormat("dd/MM/yyyy");
+		lesInfos += MSGDATE + leJour.format(laDate) + "\t";
+		SimpleDateFormat lHeure = new SimpleDateFormat("HH:mm");
+		lesInfos += MSGHEURE + lHeure.format(laDate) + "\n";
+
+		lesInfos += MSGDISPMUSCU + leComplexe.getNbPlacesRestantesMuscu() + "\t";
+		lesInfos += MSGOCCMUSCU + leComplexe.getNbPlacesOccupeesMuscu() + "\t";
+		lesInfos += MSGTXMUSCU + df2.format(leComplexe.etatMuscu()).toString() + "\t";
+		ChoixCouleur choixCouleur = new ChoixCouleur(leComplexe.etatFit());
+		lesInfos += MSGCOULMUSCU + (choixCouleur.getCouleur().toString()) + "\n";
+
+		lesInfos += MSGDISPFIT + this.getNbPlacesRestantesFit() + "\t";
+		lesInfos += MSGOCCFIT + this.nbPlacesOccupeesFit + "\t";
+		lesInfos += MSGTXFIT + df2.format(this.etatFit()) + "\t";
+		lesInfos += MSGCOULFIT + this.couleurFit() + "\n\n";
 	}
 }
 
