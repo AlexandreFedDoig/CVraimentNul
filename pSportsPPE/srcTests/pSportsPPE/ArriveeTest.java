08710c56638a7ca8a3b59581ceb07338a8f5b485
quenot <quenot@HOS2UC05.sio.fr>
2018-10-12 15:27:08 +0200

    Commit et push de la classe de test ArriveeTest finalisé

--- pSportsPPE/srcTests/pSportsPPE/ArriveeTest.java
+++ pSportsPPE/srcTests/pSportsPPE/ArriveeTest.java
@@ -14,112 +14,108 @@ import fr.rigaud.Complexe;
 
 public class ArriveeTest {
 
-	private Complexe leComplexe;
-	private Arrivee lArrivee;
-	private long horaireArrivee;
-	
-	@Before
-	public void setUp() {
-		final int NBTOTALPLACEMUSCU = 4;
-		final int NBTOTALPLACEFIT = 5;
-		final char CHOIXSPORT = 'M';
-		
-		leComplexe = new Complexe(NBTOTALPLACEMUSCU, NBTOTALPLACEFIT, "leComplexe");
-		lArrivee = new Arrivee(leComplexe, CHOIXSPORT);
-		this.horaireArrivee = Calendar.getInstance().getTimeInMillis();
-	}
-	
-	@Test
-	public void testConstructeur() {
-		final double DELTA = 0.1;
-		
-		assertEquals(leComplexe, lArrivee.getComplexe());
-		assertEquals('M', lArrivee.getChoixSport());
-		assertEquals(Calendar.getInstance().getTimeInMillis(), lArrivee.getHoraireArrivee(), DELTA);
-		assertEquals(null, lArrivee.gethDep());
-	}
+    private Complexe leComplexe;
+    private Arrivee lArrivee;
+    private long horaireArrivee;
+    private Calendar heureDepart;
+    private int numArrivee = 0;
+    private int numSortie = 0;
+    
+    @Before
+    public void setUp() {
+        final int NBTOTALPLACEMUSCU = 4;
+        final int NBTOTALPLACEFIT = 5;
+        final char CHOIXSPORT = 'M';
+        
+        leComplexe = new Complexe(NBTOTALPLACEMUSCU, NBTOTALPLACEFIT, "leComplexe");
+        lArrivee = new Arrivee(leComplexe, CHOIXSPORT);
+        this.horaireArrivee = Calendar.getInstance().getTimeInMillis();
+        this.numArrivee += 1;
+        
+    }
 
-	@Test
-	public void testAfficheBillet() {
-		final String MSGNOMCOMPLEXE = "Complexe ";
-		final String MSGNUMBILLET = "Billet d'entrée n° : ";
-		final String MSGDATE = "Date : ";
-		final String MSGHEURE = "Heure : ";
-		final String RC = "\n";
-		
-		String leBillet;
-		
-		Calendar leCal = Calendar.getInstance();
-		leCal.setTimeInMillis(horaireArrivee);
-		Date laDate = leCal.getTime();
-		SimpleDateFormat leJour = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat lHeure = new SimpleDateFormat("HH:mm");
-		
-		leBillet = MSGNOMCOMPLEXE + lArrivee.getComplexe().getNomComplexe() + "\t";
-		leBillet += MSGNUMBILLET + lArrivee.getNumeroArrivee() + RC;
-		leBillet += MSGDATE + leJour.format(laDate) + RC;
-		leBillet += MSGHEURE + lHeure.format(laDate) + RC;
-		
-		assertEquals(leBillet, lArrivee.afficheBillet());
-	}
-	
-	@Test
-	public void testAfficheTicket() {
-		final String MSGNOMCOMPLEXE = "Complexe ";
-		final String MSGNUMTICKET = "Ticket de sortie n° : ";
-		final String MSGDATE = "Date : ";
-		final String MSGHEURE = "Heure : ";
-		final String MSGMONTANT = "Montant : ";
-		final String RC = "\n";
-		
-		String leTicket;
-		Calendar heureDepart = Calendar.getInstance();
-		
-		Date laDate = heureDepart.getTime();
-		SimpleDateFormat leJour = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat lHeure = new SimpleDateFormat("HH:mm");
-		
-		leTicket = MSGNOMCOMPLEXE + lArrivee.getComplexe().getNomComplexe() + "\t";
-		leTicket += MSGNUMTICKET + lArrivee.getNumeroArrivee()+1 + RC;
-		leTicket += MSGDATE + leJour.format(laDate) + RC;
-		leTicket += MSGHEURE + lHeure.format(laDate) + RC;
-		leTicket += MSGMONTANT + lArrivee.getMontant() + "€" + RC;
-		
-		assertEquals(leTicket, lArrivee.afficheTicket());
-	}
-	
-	@Test
-	public void testGetMontant() {
-		double cout = 0;
-		final double DELTA = 1;
-		
-		//test avec l'heure de depart non instanciée
-		assertEquals(cout, lArrivee.getMontant(), DELTA);
-		
-		//test avec l'heure instanciée avec rien
-		Calendar heureDepart = Calendar.getInstance();
-		assertEquals(cout, lArrivee.getMontant(), DELTA);
-		
-		//test avec 23 min de durée
-		cout = 0.5;
-		assertEquals(cout, lArrivee.getMontant(), DELTA);
-	}
-	
-	@Test
-	public void testGetters() {
-		final double DELTA = 1;
-		
-		assertEquals(leComplexe, lArrivee.getComplexe());
-		assertEquals('M', lArrivee.getChoixSport());
-		assertEquals(Calendar.getInstance().getTimeInMillis(), lArrivee.getHoraireArrivee(), DELTA);
-		assertEquals(null, lArrivee.gethDep());
-		assertEquals(1, lArrivee.getNumeroArrivee());
-		assertEquals(1, lArrivee.getNumeroSortie());
-		
-		lArrivee.setNumeroArrivee(3);
-		assertEquals(3, lArrivee.getNumeroArrivee(), DELTA);
-	}
-	
-	
-	
+    @Test
+    public void testAfficheBillet() {
+        final String MSGNOMCOMPLEXE = "Complexe ";
+        final String MSGNUMBILLET = "Billet d'entrée n° : ";
+        final String MSGDATE = "Date : ";
+        final String MSGHEURE = "Heure : ";
+        final String RC = "\n";
+        final String NOMCOMPLEXE = "leComplexe";
+        
+        String leBillet;
+        
+        Calendar leCal = Calendar.getInstance();
+        leCal.setTimeInMillis(horaireArrivee);
+        Date laDate = leCal.getTime();
+        SimpleDateFormat leJour = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat lHeure = new SimpleDateFormat("HH:mm");
+        
+        leBillet = MSGNOMCOMPLEXE + NOMCOMPLEXE + "\t";
+        leBillet += MSGNUMBILLET + numArrivee + RC;
+        leBillet += MSGDATE + leJour.format(laDate) + RC;
+        leBillet += MSGHEURE + lHeure.format(laDate) + RC;
+        
+        assertEquals(leBillet, lArrivee.afficheBillet());
+    }
+    
+    @Test
+    public void testAfficheTicket() {
+        final String MSGNOMCOMPLEXE = "Complexe ";
+        final String MSGNUMTICKET = "Ticket de sortie n° : ";
+        final String MSGDATE = "Date : ";
+        final String MSGHEURE = "Heure : ";
+        final String MSGMONTANT = "Montant : ";
+        final String RC = "\n";
+        final String NOMCOMPLEXE = "leComplexe";
+        
+        String leTicket;
+        Calendar heureDepart = Calendar.getInstance();
+        
+        Date laDate = heureDepart.getTime();
+        SimpleDateFormat leJour = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat lHeure = new SimpleDateFormat("HH:mm");
+        
+        leTicket = MSGNOMCOMPLEXE + NOMCOMPLEXE + "\t";
+        leTicket += MSGNUMTICKET + ++numSortie + RC;
+        leTicket += MSGDATE + leJour.format(laDate) + RC;
+        leTicket += MSGHEURE + lHeure.format(laDate) + RC;
+        leTicket += MSGMONTANT + 0 + "€" + RC;
+        
+        assertEquals(leTicket, lArrivee.afficheTicket());
+    }
+    
+    @Test
+    public void testGetMontant() {
+        double cout = 0;
+        final double DELTA = 0.001;
+        
+        //test avec l'heure de depart non instanciée
+        assertEquals(cout, lArrivee.getMontant(), DELTA);
+        
+        //test avec 23 min de durée
+        cout = 0.5;
+        heureDepart = Calendar.getInstance();
+        heureDepart.add(Calendar.MINUTE, +23);
+        lArrivee.sethDep(heureDepart);
+        assertEquals(cout, lArrivee.getMontant(), DELTA);
+        
+        //test avec 47 min de durée
+        cout = 1;
+        heureDepart.add(Calendar.MINUTE, +24);
+        lArrivee.sethDep(heureDepart);
+        assertEquals(cout, lArrivee.getMontant(), DELTA);
+        
+        //test avec 1h03 de durée
+        cout = 1.5;
+        heureDepart.add(Calendar.MINUTE, +16);
+        lArrivee.sethDep(heureDepart);
+        assertEquals(cout, lArrivee.getMontant(), DELTA);
+    }
+    
+    @Test
+    public void testSetters() {
+        lArrivee.setNumeroArrivee(1);
+        assertEquals(1, lArrivee.getNumeroArrivee());
+    }
 }
